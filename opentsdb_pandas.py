f7a96f4b90b5b7ae824d2c1b4465f76a29bfb734
twwlodarczyk <tw@wlodarczyk.eu>
2014-02-28 11:08:27 +0100

    improved readme and docstring

--- opentsdb_pandas.py
+++ opentsdb_pandas.py
@@ -26,6 +26,7 @@ def ts_get(metric, start, end, tags='', agg='avg', rate=False, downsample='', ho
   
   Example useage:
   import opentsdb_pandas as opd
+  import datetime as dt
   ts1 = opd.ts_get('cipsi.test1.temperature', dt.datetime(2013, 4, 3, 14, 10), dt.datetime(2013, 4, 10, 11, 30), 'node=0024C3145172746B', hostname='opentsdb.at.your.place.edu')
   ts1
   """
